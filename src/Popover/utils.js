8348bbf2cb231a229b51be32ec8e3f3306ef806f
mrtnbroder <mrtnbroder@gmail.com>
2017-05-19 11:03:35 +0200

    fix(Popover): find offset of target and anchor elements

--- src/Popover/utils.js
+++ src/Popover/utils.js
@@ -1,5 +1,7 @@
 
-export const getOffsetLeft = (rect, horizontal) => {
+import { chain, compose, join, map, pipe } from '../utils/utils'
+
+const getOffsetLeft = (rect, horizontal) => {
   if (typeof horizontal === 'number') {
     return horizontal
   } else if (horizontal === 'center') {
@@ -10,7 +12,7 @@ export const getOffsetLeft = (rect, horizontal) => {
   return 0
 }
 
-export const getOffsetTop = (rect, vertical) => {
+const getOffsetTop = (rect, vertical) => {
   if (typeof vertical === 'number') {
     return vertical
   } else if (vertical === 'center') {
@@ -21,7 +23,31 @@ export const getOffsetTop = (rect, vertical) => {
   return 0
 }
 
-export const getTransformOriginValue = (transformOrigin) =>
-  [ transformOrigin.horizontal, transformOrigin.vertical ]
-    .map((n) => typeof n === 'number' ? `${n}px` : n)
-    .join(' ')
+const numberToPixel = (n) => typeof n === 'number' ? `${n}px` : n
+
+const getTransformOriginValue = pipe(map(numberToPixel), join(' '))
+
+const getRect = ([elem, origin]) => [elem.getBoundingClientRect(), origin]
+
+const getOffset = ([rect, origin]) => [{
+  offsetTop: getOffsetTop(rect, origin.vertical),
+  offsetLeft: getOffsetLeft(rect, origin.horizontal),
+  top: rect.top,
+  left: rect.left,
+}, origin]
+
+const getPosition = ([anchor, anchorOrigin, target, targetOrigin]) => {
+  const transformOrigin = getTransformOriginValue([
+    targetOrigin.horizontal,
+    targetOrigin.vertical,
+  ])
+
+  return {
+    position: 'absolute',
+    top: anchor.top + anchor.offsetTop - target.offsetTop,
+    left: anchor.left + anchor.offsetLeft - target.offsetLeft,
+    transformOrigin,
+  }
+}
+
+export const getStyle = compose(getPosition, chain(getOffset), map(getRect))
