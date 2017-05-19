1dd771681477afbd185cf9c2f5f0c728c6cd2141
mrtnbroder <mrtnbroder@gmail.com>
2017-05-19 11:00:52 +0200

    feat(ListItem): add button prop

--- src/ListItem/index.js
+++ src/ListItem/index.js
@@ -2,12 +2,22 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const button = (props) => props.button && `
+  transition: background-color 250ms ease-in-out;
+
+  &:hover {
+    background-color: rgba(0, 0, 0, .12);
+  }
+`
+
 const RMListItem = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-start;
   min-height: 48px;
   padding: 12px 16px;
+
+  ${button}
 `
 
 export const ListItem = ({ children, ...props }) => (
