a169c0e1ca9d67b3c1134013c7471887887484fb
mrtnbroder <mrtnbroder@gmail.com>
2017-05-19 11:01:45 +0200

    feat(Paper): use elevations utility

--- src/Paper/index.js
+++ src/Paper/index.js
@@ -2,13 +2,21 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import { elevations, elevationType } from '../styles/elevations'
+
 const RMPaper = styled.div`
   border-radius: 2px;
   background-color: #fff;
-  box-shadow:
-    0px 2px 4px -1px rgba(0, 0, 0, 0.2),
-    0px 4px 5px 0px rgba(0, 0, 0, 0.14),
-    0px 1px 10px 0px rgba(0, 0, 0, 0.12);
+  box-shadow: ${(props) => elevations[props.elevation]}
 `
 
 export const Paper = RMPaper
+
+Paper.propTypes = {
+  elevation: elevationType,
+}
+
+Paper.defaultProps = {
+  elevation: 2,
+  square: false,
+}
